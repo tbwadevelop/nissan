c7864e03527dee580f09dc383518b694d41512df
Juan <jdcb1218@gmail.com>
2018-02-26 18:41:39 -0500

    fixed release project

--- sites/all/themes/custom/nissan/js/nissan.js
+++ sites/all/themes/custom/nissan/js/nissan.js
@@ -1,23 +1,12 @@
 (function ($) {
 	Drupal.behaviors.nissan = {
 		attach: function (context, settings) {
-			$(document ).ajaxSuccess(function( event, xhr, settings ) {
-			   if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
-			   	      console.log(event);
-			   	      console.log(xhr);
-			   	      console.log(settings.url);
-
-
-			   	      var pathname = window.location.pathname; // Returns path only
-					  console.log(pathname);
-
-					  var url      = window.location.href; 
-					  console.log(url);
-			   	   //   if (xhr.status == false) {
-			   		  // //	window.location.href = 'gracias';
-			   		  // }	
-			   	}
-			});			
+			$( document ).ready(function() {
+				$("#block-block-1").appendTo("#skip-link"); 
+			});	
+			$(window).load(function(){
+			    $('.loader').fadeOut(500);
+			});					
 		}
 	};
 }(jQuery));
