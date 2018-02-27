629921bd65fc4a37ec9af5eb2e995f6665346825
Juan <jdcb1218@gmail.com>
2018-02-27 14:53:53 -0500

    validaciones

--- sites/all/modules/custom/nissan/js/redirect.js
+++ sites/all/modules/custom/nissan/js/redirect.js
@@ -2,15 +2,15 @@
 	Drupal.behaviors.request = {
 	    attach: function (context, settings) {
 			$(document ).ajaxSuccess(function( event, xhr, settings ) {
-			   if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
-  			   	      console.log(settings.url);
-  			   	      var search = settings.url.search("modal_forms/ajax/webform/1");
-			   	      console.log(search);
+			   // if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
+  			 //   	      console.log(settings.url);
+  			 //   	      var search = settings.url.search("modal_forms/ajax/webform/1");
+			   // 	      console.log(search);
 
-			   	     if (xhr.status == false && search > 1) {
-			   		   	window.location.href = 'gracias';
-			   		  }	
-			   	}
+			   // 	     if (xhr.status == false && search > 1) {
+			   // 		   	window.location.href = 'gracias';
+			   // 		  }	
+			   // 	}
 			});	
 		}
   	};
