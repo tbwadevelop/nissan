a676a1af90f943c712cacadac35d75a5d430d7c8
danilosantoalvarado <danilosantosalvarado@gmail.com>
2018-02-27 16:54:43 -0500

    modal responsi radio button

--- sites/all/themes/custom/nissan/js/nissan.js
+++ sites/all/themes/custom/nissan/js/nissan.js
@@ -3,14 +3,17 @@
 		attach: function (context, settings) {
 			$( document ).ready(function() {
 
-				// $("#block-block-1").appendTo("#skip-link"); 
-				// if ($(".webform-component--paso-1").length > 0) {
-				// 	$(".webform-component--paso-1").appendTo(".modal-content .modal-header "); 
-				// }
-				// if ($(".webform-component--paso-2").length > 0) {
-				// 	$(".webform-component--paso-2").appendTo(".modal-content .modal-header ");  
-				// 	$(".webform-component--paso-1").remove();
-				// }
+				$("#block-block-1").appendTo("#skip-link"); 
+				if ($(".webform-component--paso-1").length > 0) {
+					$(".webform-component--paso-1").appendTo(".modal-content .modal-header "); 
+				}
+				if ($(".webform-component--paso-2").length > 0) {
+					$(".webform-component--paso-2").appendTo(".modal-content .modal-header ");  
+					$(".webform-component--paso-1").remove();
+				}
+				$(".webform-component--terminos").appendTo("#skip-link"); 
+				
+				jQuery('.webform-component--terminos p button').appendTo('.form-item-submitted-he-leido-y-acepto-los-terminos-y-condiciones label');
 			});	
 			$(window).load(function(){
 			    $('.loader').fadeOut(500);
@@ -19,3 +22,4 @@
 		}
 	};
 }(jQuery));
+
