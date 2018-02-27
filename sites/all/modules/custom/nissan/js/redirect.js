b4cb2f17e60a6d85614d4de040b87db3520754f9
danilosantoalvarado <danilosantosalvarado@gmail.com>
2018-02-26 20:12:19 -0500

    movil formulario radio

--- sites/all/modules/custom/nissan/js/redirect.js
+++ sites/all/modules/custom/nissan/js/redirect.js
@@ -3,7 +3,10 @@
 	    attach: function (context, settings) {
 			$(document ).ajaxSuccess(function( event, xhr, settings ) {
 			   if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
+  			   	      console.log(settings.url);
   			   	      var search = settings.url.search("modal_forms/ajax/webform/1");
+			   	      console.log(search);
+
 			   	     if (xhr.status == false && search > 1) {
 			   		   	window.location.href = 'gracias';
 			   		  }	
