014515eb5696b55f66b9272014e02243f68869e2
Juan <jdcb1218@gmail.com>
2018-02-26 20:57:35 -0500

    js

--- sites/all/themes/custom/nissan/js/nissan.js
+++ sites/all/themes/custom/nissan/js/nissan.js
@@ -3,6 +3,16 @@
 		attach: function (context, settings) {
 			$( document ).ready(function() {
 				$("#block-block-1").appendTo("#skip-link"); 
+
+				if ($(".webform-component--paso-1").length > 0) {
+					$(".webform-component--paso-1").appendTo(".modal-content .modal-header "); 
+				}
+
+				if ($(".webform-component--paso-2").length > 0) {
+					$(".webform-component--paso-2").appendTo(".modal-content .modal-header ");  
+					$(".webform-component--paso-1").remove(); $(".webform-component--paso-2").remove(); 
+				}
+
 			});	
 			$(window).load(function(){
 			    $('.loader').fadeOut(500);
