27a48a2164b5282debddd1f41270a318ed94b0b2
Juan <jdcb1218@gmail.com>
2018-02-28 11:34:20 -0500

    Fixed

--- sites/all/modules/custom/nissan/js/redirect.js
+++ sites/all/modules/custom/nissan/js/redirect.js
@@ -2,17 +2,13 @@
 	Drupal.behaviors.request = {
 	    attach: function (context, settings) {
 			$(document ).ajaxSuccess(function( event, xhr, settings ) {
-
-				$(".front .vjs-control-text").text("Play");
-			   // if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
-  			 //   	      console.log(settings.url);
-  			 //   	      var search = settings.url.search("modal_forms/ajax/webform/1");
-			   // 	      console.log(search);
-
-			   // 	     if (xhr.status == false && search > 1) {
-			   // 		   	window.location.href = 'gracias';
-			   // 		  }	
-			   // 	}
+			   $(".front .vjs-control-text").text("Play");
+			   if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
+  			   	   var search = settings.url.search("modal_forms/ajax/webform/1");
+		   	         if (xhr.status == false && search > 1) {
+			   		   	window.location.href = 'gracias';
+			   		  }	
+			   	}
 			});	
 		}
   	};
