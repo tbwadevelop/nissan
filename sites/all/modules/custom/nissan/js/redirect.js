9e3c684ed83d7023e1f7e67452e47dd2ca9f6b93
Juan <jdcb1218@gmail.com>
2018-03-01 18:46:42 -0500

    Text PLAY Video

--- sites/all/modules/custom/nissan/js/redirect.js
+++ sites/all/modules/custom/nissan/js/redirect.js
@@ -2,7 +2,7 @@
 	Drupal.behaviors.request = {
 	    attach: function (context, settings) {
 			$(document ).ajaxSuccess(function( event, xhr, settings ) {
-			   $(".front .vjs-control-text").text("Play");
+			   $(".front .vjs-control-text").text("PLAY");
 			   if ( $(".webform-client-form-1 .form-actions .webform-submit").length > 0 && $(".webform-client-form-1 .form-managed-file .file").length > 0) {
   			   	   var search = settings.url.search("modal_forms/ajax/webform/1");
 		   	         if (xhr.status == false && search > 1) {
